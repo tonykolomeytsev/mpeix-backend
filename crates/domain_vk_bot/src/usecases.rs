3a1b5b3318835ace941273b8e1fb89c7ccc55300
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2023-02-12 15:46:33 +0300

    Fix group chat replies for vk

--- crates/domain_vk_bot/src/usecases.rs
+++ crates/domain_vk_bot/src/usecases.rs
@@ -1,19 +1,25 @@
 use anyhow::Context;
 use common_errors::errors::CommonError;
 
-use crate::{vk_api::VkApi, Keyboard};
+use crate::{vk_api::VkApi, Keyboard, MessagePeerType};
 
 /// Send message reply to VK
 #[derive(Default)]
 pub struct ReplyToVkUseCase(VkApi);
 
 impl ReplyToVkUseCase {
-    pub async fn reply(&self, text: &str, peer_id: i64, keyboard: &Keyboard) -> anyhow::Result<()> {
+    pub async fn reply(
+        &self,
+        text: &str,
+        peer_id: i64,
+        peer_type: &MessagePeerType,
+        keyboard: &Keyboard,
+    ) -> anyhow::Result<()> {
         let keyboard = serde_json::to_string(keyboard).with_context(|| {
             CommonError::internal("Error while serializing vk keyboard to JSON")
         })?;
         self.0
-            .send_message(text, peer_id, Some(&[("keyboard", &keyboard)]))
+            .send_message(text, peer_id, peer_type, Some(&[("keyboard", &keyboard)]))
             .await
     }
 }
--- crates/domain_vk_bot/src/vk_api.rs
+++ crates/domain_vk_bot/src/vk_api.rs
@@ -4,6 +4,8 @@ use anyhow::{anyhow, Context};
 use common_errors::errors::CommonError;
 use reqwest::{redirect::Policy, Client, ClientBuilder};
 
+use crate::MessagePeerType;
+
 const VK_API_VERSION: &str = "5.130";
 
 /// Representation of the VK API
@@ -34,6 +36,7 @@ impl VkApi {
         &self,
         text: &str,
         peer_id: i64,
+        peer_type: &MessagePeerType,
         additional_query: Option<&[(&str, &str)]>,
     ) -> anyhow::Result<()> {
         let mut request = self
@@ -43,7 +46,14 @@ impl VkApi {
                 ("v", VK_API_VERSION),
                 ("access_token", &self.access_token),
                 ("random_id", &rand::random::<i32>().to_string()),
-                ("peer_id", &peer_id.to_string()),
+                (
+                    if matches!(peer_type, MessagePeerType::GroupChat) {
+                        "chat_id"
+                    } else {
+                        "peer_id"
+                    },
+                    &peer_id.to_string(),
+                ),
                 ("message", text),
             ]);
         if let Some(query) = additional_query {
