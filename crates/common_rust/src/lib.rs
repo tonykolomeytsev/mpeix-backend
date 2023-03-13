201dab2819cf98bc474692d841a018c8a1f948da
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2023-03-13 23:02:44 +0300

    Add inline attribute to env var functions

--- crates/common_rust/src/lib.rs
+++ crates/common_rust/src/lib.rs
@@ -3,18 +3,21 @@ pub mod env {
 
     /// Get environment variable value if exists.
     /// In case of errors return [Option::None].
+    #[inline]
     pub fn get(key: &str) -> Option<String> {
         std::env::var(key).ok()
     }
 
     /// Get environment variable value if exists.
     /// In case of errors return `default`.
+    #[inline]
     pub fn get_or<S: AsRef<str>>(key: &str, default: S) -> String {
         std::env::var(key).unwrap_or_else(|_| default.as_ref().to_owned())
     }
 
     /// Get environment variable value if exists, then parse it to necessary type.
     /// In case of errors return [Option::None].
+    #[inline]
     pub fn get_parsed<T: FromStr>(key: &str) -> Option<T> {
         match std::env::var(key) {
             Ok(var) => var.parse::<T>().ok(),
@@ -24,6 +27,7 @@ pub mod env {
 
     /// Get environment variable value if exists, then parse it to necessary type.
     /// In case of errors return `default`.
+    #[inline]
     pub fn get_parsed_or<T: FromStr>(key: &str, default: T) -> T {
         match std::env::var(key) {
             Ok(var) => var.parse::<T>().unwrap_or(default),
@@ -33,6 +37,7 @@ pub mod env {
 
     /// Get environment variable value if exists,
     /// or panic with readable description.
+    #[inline]
     pub fn required(key: &str) -> String {
         std::env::var(key).unwrap_or_else(|_| panic!("Environment variable {key} not provided"))
     }
